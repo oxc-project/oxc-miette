792029d68a6e33691ef025402a11cda4a0370ffb
Boshen <boshenc@gmail.com>
2026-08-09 10:14:34 +0800

    refactor(reporting): remove unused rendering state (#305)

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -4,8 +4,9 @@ Reporters included with `miette`.
 
 pub use graphical::*;
 pub use json::*;
-pub use theme::*;
+pub use theme::GraphicalTheme;
 
 mod graphical;
 mod json;
-mod theme;
+#[expect(clippy::redundant_pub_crate, reason = "prevents public glob re-export")]
+pub(crate) mod theme;
